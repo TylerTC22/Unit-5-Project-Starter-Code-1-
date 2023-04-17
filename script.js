0291e86a9f94e8794ae470e0674612ce7573f425
TylerTC22 <121177890+TylerTC22@users.noreply.github.com>
2023-04-17 13:42:33 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let firstBox = document.querySelector('.input1')
+let secondBox = document.querySelector('.input2')
+let button = document.querySelector('button')
+let answer1 = document.querySelector('.result')
+
+button.onclick = function(){
+  let userInput1 = firstBox.value
+  let userInput2 = secondBox.value
+  //console.log(userInput)
+}
+
+else if(userInput1 >50 && userInput2 == "really strong"){
+  answer1.innerHTML = "Aaron Donald"
+}
+else if(userInput1 >50 && userInput2 == "really fast"){
+  answer1.innerHTML = "Tyreek Hill"
+}
+}
+
